d0dd4893c774af0a9cca93c9005a922370ac6379
Konrad Paluszek <kezsulap@gmail.com>
2022-08-31 23:25:30 +0200

    Add pychess puzzler

--- user-script.js
+++ user-script.js
@@ -8,6 +8,7 @@
 // @match        https://pychess-variants-pr-657.herokuapp.com/*
 // @match        https://liantichess.herokuapp.com/*
 // @match        https://playstrategy.org/*
+// @match        https://pychess-pr-657.herokuapp.com/*
 // @match        127.0.0.1:8080/*
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
